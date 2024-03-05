d025136cf8f1298cf4973951ac3d7034cfc2947e
Mariya Podosinova <83884517+mariya-podosinova@users.noreply.github.com>
2024-03-05 11:23:50 +0000

    mock api setup

---
+++ src/api/recipeApi.js
@@ -0,0 +1,24 @@
+import { handleResponse, handleError } from "./apiUtils";
+const baseUrl = process.env.REACT_APP_API_URL + "/recipes/";
+
+export function getRecipes() {
+  return fetch(baseUrl)
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function saveRecipe(recipe) {
+  return fetch(baseUrl + (recipe.id || ""), {
+    method: recipe.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(recipe)
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function deleteRecipe(recipeId) {
+  return fetch(baseUrl + recipeId, { method: "DELETE" })
+    .then(handleResponse)
+    .catch(handleError);
+}
