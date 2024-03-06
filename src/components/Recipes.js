e365854c419df3d9cbaa9a5a3bf93f47ffec20fb
Mariya Podosinova <83884517+mariya-podosinova@users.noreply.github.com>
2024-03-06 10:19:20 +0000

    rmove inline manage recipe form

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,32 +6,20 @@ import PropTypes from 'prop-types'
 function Recipes ({recipes, createRecipe}) {
     console.log('Recipes, createRecipe:', { recipes, createRecipe });
 
-const [recipeTitle, setRecipeTitle] = useState('')
 
-function handleChange(e){
-    setRecipeTitle(e.target.value)
-}
-function handleSubmit(e){
-    console.log('recipeTitle', recipeTitle);
-    e.preventDefault()
-    if (recipeTitle) {
-        createRecipe({ id: Date.now(), title: recipeTitle });
-        setRecipeTitle('');
-    }
-}
+
     return (
 
-        <form onSubmit={handleSubmit}>
+        <>
             <h2>Recipes</h2>
             <h3>Add recipe</h3>
-            <input type="text" onChange={handleChange} value={recipeTitle} />
-            <input type="submit" value='Save' />
+   
             <ul>
                 {recipes.map(recipe=>(
                     <li key={recipe.id}>{recipe.title}</li>
                 ))}
             </ul>
-        </form>
+        </>
 
     )
 
