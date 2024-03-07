080a48563b220d3a8a939aa2c36cf23e856280db
Mariya Podosinova <83884517+mariya-podosinova@users.noreply.github.com>
2024-03-07 14:56:15 +0000

    add button to redirect to add new article page

--- src/components/RecipesPage.js
+++ src/components/RecipesPage.js
@@ -1,15 +1,19 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector } from 'react-redux';
 import { fetchRecipes } from '../redux/actions/recipeActions'
 import { fetchAuthors } from '../redux/actions/authorActions'
 import RecipeList from "./RecipeList";
+import { useNavigate } from 'react-router-dom';
 
 
 function RecipesPage () {
  
-
+  const [redirectToAddRecipePage, setRedirectToAddRecipePage] = useState(false);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
+    const recipes = useSelector(state => state.recipes)
+    const authors = useSelector(state => state.authors)
     useEffect(() => {
       dispatch(fetchRecipes());
     }, [dispatch]);
@@ -19,13 +23,22 @@ function RecipesPage () {
       }, [dispatch]);
   
 
-    const recipes = useSelector(state => state.recipes)
-    const authors = useSelector(state => state.authors)
-   
+    function handleAddArticle() {
+      navigate("/recipe"); 
+    }
 
     return (
         <>
+
             <h2 className="text-light">Articles</h2>
+
+            <button
+          className="btn btn-lg btn-dark custom-width"
+          onClick={handleAddArticle}
+        >
+          Add New
+        </button>
+
             <RecipeList recipes={recipes} authors={authors}/>
         </>
     )
