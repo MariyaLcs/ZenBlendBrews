3ae905db7f4512b9ed292fdf4cb7cd8a87088f16
Mariya Podosinova <83884517+mariya-podosinova@users.noreply.github.com>
2024-03-08 11:51:19 +0000

    add spinner component

--- src/components/ManageArticlePage.js
+++ src/components/ManageArticlePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRecipes, saveRecipeAction } from '../redux/actions/recipeActions';
 import { fetchAuthors } from '../redux/actions/authorActions';
@@ -6,58 +6,63 @@ import ArticleForm from './ArticleForm';
 import { useParams, useNavigate } from 'react-router-dom';
 
 function ManageArticlePage() {
-    const navigate = useNavigate();
-    const { slug } = useParams();
+  const navigate = useNavigate();
+  const { slug } = useParams();
 
-    const recipes = useSelector(state => state.recipes);
-    const authors = useSelector(state => state.authors);
+  const recipes = useSelector((state) => state.recipes);
+  const authors = useSelector((state) => state.authors);
 
-    const [recipe, setRecipe] = useState({});
-    const [errors, setErrors] = useState({});
+  const [recipe, setRecipe] = useState({});
+  const [errors, setErrors] = useState({});
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      if (recipes.length === 0) {
-        dispatch(fetchRecipes());
-      }
-    }, [recipes.length, dispatch]);
-
-    useEffect(() => {
-      if (authors.length === 0) {
-        dispatch(fetchAuthors());
-      }
-    }, [authors.length, dispatch]);
-
-    useEffect(() => {
-
-        const existingRecipe = recipes.find(r => r.slug === slug);
-        setRecipe(existingRecipe || {});
-      
-    }, [slug, recipes]);
-
-    function handleChange(e) {
-        const { name, value } = e.target;
-        setRecipe(prevRecipe => ({
-          ...prevRecipe, 
-          [name]: name === 'authorId' ? parseInt(value, 10) : value
-        }));
+  useEffect(() => {
+    if (recipes.length === 0) {
+      dispatch(fetchRecipes());
     }
+  }, [recipes.length, dispatch]);
 
-    function handleSave(e) {
-        e.preventDefault(); 
-        if (recipe) {
-          dispatch(saveRecipeAction(recipe));
-          navigate('/recipes');
-        }
+  useEffect(() => {
+    if (authors.length === 0) {
+      dispatch(fetchAuthors());
     }
+  }, [authors.length, dispatch]);
 
- 
-    return (
-        <div>
-            {recipe && <ArticleForm recipe={recipe} authors={authors} errors={errors} onChange={handleChange} onSave={handleSave}/>}
-        </div>
-    );
+  useEffect(() => {
+    const existingRecipe = recipes.find((r) => r.slug === slug);
+    setRecipe(existingRecipe || {});
+  }, [slug, recipes]);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setRecipe((prevRecipe) => ({
+      ...prevRecipe,
+      [name]: name === 'authorId' ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function handleSave(e) {
+    e.preventDefault();
+    if (recipe) {
+      dispatch(saveRecipeAction(recipe));
+      navigate('/recipes');
+    }
+  }
+
+  return (
+    <div>
+      {recipe && (
+        <ArticleForm
+          recipe={recipe}
+          authors={authors}
+          errors={errors}
+          onChange={handleChange}
+          onSave={handleSave}
+        />
+      )}
+    </div>
+  );
 }
 
 export default ManageArticlePage;
--- src/components/RecipesPage.js
+++ src/components/RecipesPage.js
@@ -1,47 +1,43 @@
-import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector } from 'react-redux';
-import { fetchRecipes } from '../redux/actions/recipeActions'
-import { fetchAuthors } from '../redux/actions/authorActions'
-import RecipeList from "./RecipeList";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRecipes } from '../redux/actions/recipeActions';
+import { fetchAuthors } from '../redux/actions/authorActions';
+import RecipeList from './RecipeList';
 import { useNavigate } from 'react-router-dom';
+import Spinner from './common/Spinner';
 
+function RecipesPage() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-function RecipesPage () {
- 
-  const [redirectToAddRecipePage, setRedirectToAddRecipePage] = useState(false);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const recipes = useSelector((state) => state.recipes);
+  const authors = useSelector((state) => state.authors);
+  useEffect(() => {
+    dispatch(fetchRecipes());
+  }, [dispatch]);
 
-    const recipes = useSelector(state => state.recipes)
-    const authors = useSelector(state => state.authors)
-    useEffect(() => {
-      dispatch(fetchRecipes());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchAuthors());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(fetchAuthors());
-      }, [dispatch]);
-  
+  function handleAddArticle() {
+    navigate('/recipe');
+  }
 
-    function handleAddArticle() {
-      navigate("/recipe"); 
-    }
+  return (
+    <>
+      <h2 className="text-light">Articles</h2>
+      <Spinner />
+      <button
+        className="btn btn-lg btn-dark custom-width"
+        onClick={handleAddArticle}
+      >
+        Add New
+      </button>
 
-    return (
-        <>
-
-            <h2 className="text-light">Articles</h2>
-
-            <button
-          className="btn btn-lg btn-dark custom-width"
-          onClick={handleAddArticle}
-        >
-          Add New
-        </button>
-
-            <RecipeList recipes={recipes} authors={authors}/>
-        </>
-    )
+      <RecipeList recipes={recipes} authors={authors} />
+    </>
+  );
 }
 
 export default RecipesPage;
