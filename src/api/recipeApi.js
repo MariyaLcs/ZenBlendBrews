16f3689ffb9fd743d931ab6b036a9304ceeeda96
Mariya Podosinova <83884517+mariya-podosinova@users.noreply.github.com>
2024-03-11 11:37:38 +0000

    convert to async/await

--- src/api/recipeApi.js
+++ src/api/recipeApi.js
@@ -1,25 +1,34 @@
 import { handleResponse, handleError } from './apiUtils';
 const baseUrl = process.env.REACT_APP_API_URL + '/recipes/';
 
-export function getRecipes() {
-  return fetch(baseUrl).then(handleResponse).catch(handleError);
+export async function getRecipes() {
+  try {
+    const response = await fetch(baseUrl);
+    return handleResponse(response);
+  } catch (error) {
+    return handleError(error);
+  }
 }
 
-export function saveRecipe(recipe) {
-  return fetch(baseUrl + (recipe.id || ''), {
-    method: recipe.id ? 'PUT' : 'POST',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify(recipe),
-  })
-    .then(handleResponse)
-    .catch((error) => {
-      handleError(error);
-      return Promise.reject(error);
+export async function saveRecipe(recipe) {
+  try {
+    const response = await fetch(baseUrl + (recipe.id || ''), {
+      method: recipe.id ? 'PUT' : 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(recipe),
     });
+    return handleResponse(response);
+  } catch (error) {
+    handleError(error);
+    return await Promise.reject(error);
+  }
 }
 
-export function deleteRecipe(recipeId) {
-  return fetch(baseUrl + recipeId, { method: 'DELETE' })
-    .then(handleResponse)
-    .catch((error) => Promise.reject(error));
+export async function deleteRecipe(recipeId) {
+  try {
+    const response = await fetch(baseUrl + recipeId, { method: 'DELETE' });
+    return handleResponse(response);
+  } catch (error) {
+    return await Promise.reject(error);
+  }
 }
