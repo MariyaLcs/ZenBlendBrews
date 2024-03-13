b775c576f1011170becbacedc2bf5badc3d0e449
Mariya Podosinova <mariya.podosinova@gmail.com>
2024-03-13 08:09:53 +0000

    changes for github pages2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
-import PageNotFound from './components/PageNotFound';
-import RecipesPage from './components/RecipesPage';
-import Header from './components/Header';
-import ManageArticlePage from './components/ManageArticlePage';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import PageNotFound from "./components/PageNotFound";
+import RecipesPage from "./components/RecipesPage";
+import Header from "./components/Header";
+import ManageArticlePage from "./components/ManageArticlePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
             <Route path="/recipes" element={<RecipesPage />} />
             <Route path="/recipe/:slug" element={<ManageArticlePage />} />
             <Route path="/recipe" element={<ManageArticlePage />} />
-            <Route path="*" element={<PageNotFound />} />
+            <Route path="*" element={<RecipesPage />} />
           </Routes>
           <ToastContainer autoClose={3000} hideProgressBar />
         </div>
